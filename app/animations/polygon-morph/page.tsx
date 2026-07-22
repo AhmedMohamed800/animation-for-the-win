ea0da22e77607af8010cf9e74a813669726f78aa
AhmedMohamed800 <ahmedmoh0107@gmail.com>
2026-07-22 23:44:50 +0300

    feat: create polygon-morph animatoin

---
+++ app/animations/polygon-morph/page.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import Image from "next/image";
+
+gsap.registerPlugin(useGSAP);
+
+export default function PolygonMorph() {
+  const rectElement = useRef<HTMLDivElement | null>(null);
+  const foxRef = useRef<HTMLImageElement | null>(null);
+  const pandaRef = useRef<HTMLImageElement | null>(null);
+  const gooseRef = useRef<HTMLImageElement | null>(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({
+        repeat: -1,
+      });
+
+      tl.to(
+        rectElement.current,
+        {
+          ease: "power1.inOut",
+          clipPath:
+            "polygon(57% 0%, 100% 74%, 100% 100%, 73% 100%, 0% 57%, 0% 41%, 0% 0%)",
+        },
+        "+=2",
+      ).to(
+        foxRef.current,
+        {
+          opacity: 0,
+          ease: "power2.out",
+          duration: 0.3,
+        },
+        "<",
+      );
+
+      tl.to(
+        rectElement.current,
+        {
+          ease: "power1.inOut",
+
+          clipPath:
+            "polygon(100% 0%, 87% 74%, 82% 100%, 52% 100%, 0% 65%, 0% 47%, 48% 0%)",
+        },
+        "+=2",
+      ).to(
+        pandaRef.current,
+        {
+          opacity: 0,
+          ease: "power2.out",
+          duration: 0.3,
+        },
+        "<",
+      );
+
+      tl.to(
+        rectElement.current,
+        {
+          ease: "power1.inOut",
+
+          clipPath:
+            "polygon(100% 0%, 100% 16%, 56% 100%, 43% 100%, 0% 57%, 0% 41%, 49% 0%)",
+        },
+        "+=2",
+      ).to(
+        foxRef.current,
+        {
+          opacity: 1,
+          ease: "power2.out",
+          duration: 0.3,
+        },
+        "<",
+      );
+    },
+    { scope: rectElement },
+  );
+
+  return (
+    <main className="flex flex-col gap-8 justify-center items-center min-h-screen">
+      <h1 className="text-2xl uppercase font-bold">Polygon Morph</h1>
+      <div
+        className="flex relative  justify-center items-center h-100 w-90 md:w-150 "
+        style={{
+          clipPath:
+            "polygon(100% 0%, 100% 16%, 56% 100%, 43% 100%, 0% 57%, 0% 41%, 49% 0%)",
+        }}
+        ref={rectElement}
+      >
+        <Image
+          ref={foxRef}
+          src="/polygon-morph/fox.webp"
+          alt="fox"
+          width={0}
+          height={0}
+          className=" absolute inset-0 z-3"
+          sizes="100vw"
+          style={{ width: "100%", height: "100%" }}
+        />
+        <Image
+          ref={pandaRef}
+          src="/polygon-morph/panda.webp"
+          alt="panda"
+          width={0}
+          height={0}
+          className=" absolute inset-0 z-2"
+          sizes="100vw"
+          style={{ width: "100%", height: "100%" }}
+        />
+        <Image
+          ref={gooseRef}
+          src="/polygon-morph/goose.webp"
+          alt="goose"
+          width={0}
+          height={0}
+          className=" absolute inset-0 z-1"
+          sizes="100vw"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+    </main>
+  );
+}
